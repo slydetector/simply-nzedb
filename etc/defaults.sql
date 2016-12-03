2ad62aea4b4368716c940e25cd546fac99231dc2
slydetector <theslydetector@gmail.com>
2016-12-02 23:13:40 -0600

    Turn on rar password checking - needed for jpg and mediainfo postprocessing

--- etc/defaults.sql
+++ etc/defaults.sql
@@ -3,12 +3,14 @@ update settings set value="/usr/bin/ffmpeg" where name="ffmpegpath";
 update settings set value="/usr/local/bin/mediainfo" where name="mediainfopath";
 update settings set value="/usr/bin/7za" where name="7zippath";
 update settings set value="/usr/bin/timeout" where name="timeoutpath";
+# Has to be turned on for the rest of "additional postprocessing" to work
+update settings set value=1 where name="checkpasswordedrar";
 update settings set value=1 where name="processjpg";
 update settings set value=1 where name="processthumbnails";
 update settings set value=1 where name="compressedheaders";
 select name, value from settings where name in (
   'unrarpath', 'ffmpegpath', 'mediainfopath', '7zippath', 'timeoutpath', 'processjpg', 
-  'processthumbnails', 'compressedheaders');
+  'processthumbnails', 'compressedheaders', 'checkpasswordedrar');
 
 update groups set active=1 where name="alt.binaries.teevee";
 
